76df84a94d008cc37891e83748a6eb33a3d26b0b
ArtemTeleluhin <teleluhin.artem@yandex.ru>
2022-04-09 20:34:50 +0300

    Add converter_to_csv and do converting data from database to csv file

---
+++ converter_to_csv.py
@@ -0,0 +1,34 @@
+import sqlite3
+import csv
+
+DATABASE_NAME = "attractions_base.db"
+CSV_NAME = "attractions_list.csv"
+
+
+def convert_to_csv(database_name, csv_name):
+    con = sqlite3.connect(database_name)
+    database = con.cursor()
+    data = list(database.execute("SELECT * FROM articles_tambov_2").fetchall())
+    con.close()
+    dict_list = []
+    for elem in data:
+        dict_list.append({
+            'id': elem[0],
+            'title': elem[1],
+            'url': elem[2],
+            'latitude': elem[3],
+            'longitude': elem[4],
+            'is_correctly': elem[5]
+        })
+    with open(csv_name, 'w') as file:
+        writer = csv.DictWriter(file, fieldnames=list(dict_list[0].keys()), quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        for d in dict_list:
+            writer.writerow(d)
+
+
+def print_from_csv(csv_name):
+    with open(csv_name) as file:
+        reader = csv.DictReader(file)
+        for row in list(reader)[:10]:
+            print(dict(row))
