4bae51f5236d87a9c74402e354666ae4bf385d5b
ArtemTeleluhin <teleluhin.artem@yandex.ru>
2022-04-10 19:28:24 +0300

    Debug: now numbers from csv file are converted to numbers (int and float), corrected mistake ('dict' to 'dist')

--- db_csv_manager.py
+++ db_csv_manager.py
@@ -9,10 +9,15 @@ class DBManager:
         with open(csv_name) as file:
             reader = csv.DictReader(file)
             self.data = [dict(elem) for elem in reader]
+        for elem in self.data:
+            elem['id'] = int(elem['id'])
+            elem['latitude'] = float(elem['latitude'])
+            elem['longitude'] = float(elem['longitude'])
+            elem['is_correctly'] = bool(elem['is_correctly'])
 
     def sort_by_distance(self, latitude, longitude):
         result = deepcopy(self.data)
         for elem in result:
-            elem['dict'] = count_distance(latitude, longitude, elem['latitude'], elem['longitude'])
-        result.sort(key=lambda elem: elem['dict'])
+            elem['dist'] = count_distance(latitude, longitude, elem['latitude'], elem['longitude'])
+        result.sort(key=lambda elem: elem['dist'])
         return result
